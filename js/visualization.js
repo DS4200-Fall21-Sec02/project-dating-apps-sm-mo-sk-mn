6a3eb790eef2ac23b787ff97e6035dcc020a077e
Marta Navascués <martanavascues@big-bby-de-marts.local>
2021-11-07 20:56:09 -0500

    test1

--- js/visualization.js
+++ js/visualization.js
@@ -1,7 +1,7 @@
 // Immediately Invoked Function Expression to limit access to our 
 // variables and prevent 
-((() => {
+d3.csv("Copy of Online Dating responses - Form Responses 1.csv").then((data) => {
 
   console.log('Hello, world!');
 
-})());
+});

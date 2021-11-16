05b04afd259d5b707ed8df5514dca03af8aa662d
Marta Navascués <martanavascues@bigbbydemarts.myfiosgateway.com>
2021-11-16 00:47:16 -0500

    Delivery 2

--- js/visualization.js
+++ js/visualization.js
@@ -281,3 +281,74 @@ d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/da
         .attr("height", function(d) { return y(d[0]) - y(d[1]); })
         .attr("width",x.bandwidth())
 })
+
+// Vis 3
+// set the dimensions and margins of the graph
+var width = 450,
+    height = 450,
+    margin = 40;
+
+// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+var radius = Math.min(width, height) / 2 - margin;
+
+// append the svg object to the div
+var svg = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", `translate(${width/2}, ${height/2})`);
+
+// append the svg object to the div
+var svg2 = d3.select("#my_dataviz")
+.append("svg")
+  .attr("width", width)
+  .attr("height", height)
+.append("g")
+  .attr("transform", `translate(${width/2}, ${height/2})`);
+
+// Set colors
+
+const color = d3.scaleOrdinal()
+  //.domain(Object.keys(data))
+  .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#b12345", "#c21434"])
+
+// Create pie
+const pie = d3.pie()
+  .value(function(d) {return d.Count})
+
+d3.csv('data/Turn-Ons.csv').then(function(data){
+  
+// Build the pie chart where each part of the pie is a path that we build using the arc function.
+svg
+  .selectAll('whatever')
+  .data(pie(data))
+  .join('path')
+  .attr('d', d3.arc()
+    .innerRadius(0)
+    .outerRadius(radius)
+  )
+  .attr('fill', function(d){ return(color(d.data.Categories)) })
+  .attr("stroke", "black")
+  .style("stroke-width", "2px")
+  .style("opacity", 0.7)
+
+})
+
+d3.csv('data/Turn-Offs.csv').then(function(data){
+  
+// Build the pie chart where each part of the pie is a path that we build using the arc function.
+svg2
+  .selectAll('whatever')
+  .data(pie(data))
+  .join('path')
+  .attr('d', d3.arc()
+    .innerRadius(0)
+    .outerRadius(radius)
+  )
+  .attr('fill', function(d){ return(color(d.data.Categories)) })
+  .attr("stroke", "black")
+  .style("stroke-width", "2px")
+  .style("opacity", 0.7)
+
+})

1869a3cd13e05da75163de02842e6aa88f9b2dba
simrankalita <kalita.si@northeastern.edu>
2021-11-23 12:10:58 -0500

    testing

--- js/visualization.js
+++
@@ -1,7 +0,0 @@
-// Immediately Invoked Function Expression to limit access to our 
-// variables and prevent 
-d3.csv("data/Copy of Online Dating responses - Form Responses 1.csv").then((data) => {
-
-  console.log(data.slice(0, 11));
-
-});

e351e4ce9c92ccaa8003d617923efdf1068d35ac
simrankalita <kalita.si@northeastern.edu>
2021-11-30 12:02:41 -0500

    test

--- js/meetups.js
+++ js/meetups.js
@@ -1,52 +1,52 @@
 // set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 20, left: 50},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+const m = {top: 10, right: 30, bottom: 20, left: 50},
+    wi = 460 - m.left - m.right,
+    he = 400 - m.top - m.bottom;
 
 
 // append the svg4 object to the body of the page
-var svg4 = d3.select("#my_dataviz")
+const svg4 = d3.select("#my_dataviz")
   .append("svg4")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+    .attr("width", wi + m.left + m.right)
+    .attr("height", he + m.top + m.bottom)
   .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform",`translate(${m.left},${m.top})`);
 
 // Parse the Data
-d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRKwzteCb-m58OMLtu4EGBVm7g0uYB5D377RCNIfjPKRNtC6o84EbQhif1zyi0uyGA-1FjytCdsbBP6/pub?gid=1069853049&single=true&output=csv", function(data) {
+d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRKwzteCb-m58OMLtu4EGBVm7g0uYB5D377RCNIfjPKRNtC6o84EbQhif1zyi0uyGA-1FjytCdsbBP6/pub?gid=1069853049&single=true&output=csv").then(function(data) {
 
+console.log(data.columns)
   // List of subgroups = header of the csv files = gender here
-  var subgroups = data.columns.slice(0)
+  const subgroups = data.columns.slice(1)
 
   // List of groups = Amount of Meetups here = value of the first column called Meetups -> I show them on the X axis
-  var groups = d3.map(data, function(d){return(d.Meetups)}).keys()
+  const groups = data.map(d => (d.group))
 
   // Add X axis
-  var x = d3.scaleBand()
+  const x = d3.scaleBand()
       .domain(groups)
-      .range([0, width])
+      .range([0, wi])
       .padding([0.2])
 
   svg4.append("g")
-    .attr("transform", "translate(0," + height + ")")
+    .attr("transform", `translate(0, ${he})`)
     .call(d3.axisBottom(x).tickSizeOuter(0));
 
   // Add Y axis
-  var y = d3.scaleLinear()
+  const y = d3.scaleLinear()
     .domain([0, 120])
-    .range([ height, 0 ]);
+    .range([ he, 0 ]);
 
   svg4.append("g")
     .call(d3.axisLeft(y));
 
   // color palette = one color per subgroup
-  var color = d3.scaleOrdinal()
+  const color = d3.scaleOrdinal()
     .domain(subgroups)
     .range(['#e41a1c','#377eb8'])
 
   //stack the data? --> stack per subgroup
-  var stackedData = d3.stack()
+  const stackedData = d3.stack()
     .keys(subgroups)
     (data)
 
@@ -56,15 +56,15 @@ d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRKwzteCb-m58OMLtu4EGBVm
     .selectAll("g")
     // Enter in the stack data = loop key per key = group per group
     .data(stackedData)
-    .enter().append("g")
-      .attr("fill", function(d) { return color(d.key); })
+    .join("g")
+      .attr("fill", d => color(d.key))
       .selectAll("rect")
       // enter a second time = loop subgroup per subgroup to add all rectangles
-      .data(function(d) { return d; })
-      .enter().append("rect")
-        .attr("x", function(d) { return x(d.data.Meetups); })
-        .attr("y", function(d) { return y(d[1]); })
-        .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+      .data(d => d)
+      .join("rect")
+        .attr("x", d => x(d.data.group))
+        .attr("y", d => y(d[1]))
+        .attr("height", d => y(d[0]) - y(d[1]))
         .attr("width",x.bandwidth())
 
 })

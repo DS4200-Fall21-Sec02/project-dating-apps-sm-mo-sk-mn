c994345331b03f56adfce8b940066d83f5a4eacf
Marta Navascués <martanavascues@bigbbydemarts.myfiosgateway.com>
2021-12-01 23:30:14 -0500

    updates3

--- js/meetups.js
+++ js/meetups.js
@@ -15,9 +15,10 @@ var svg4 = d3.select("#vis-holder-1")
     .attr("transform", "translate(" + m.left + "," + m.top + ")");
 
 
-// Parse the Data
+// Obtain the Data
 d3.csv("data/FinalMatchesPerDay.csv").then(function(data) {
 
+// Create tooltip 
 var tooltip2 = d3.select('#my_dataviz')                               
   .append('div')    
   .style('opacity','0')                                            
@@ -33,13 +34,13 @@ tooltip2.append('div')
   .attr('class', 'count');
 
   // List of subgroups = header of the csv files = soil condition here
-  const subgroups = data.columns.slice(1)
+  var subgroups = data.columns.slice(1)
 
   // List of groups = value of the first column called group -> I show them on the X axis
-  const groups = data.map(d => (d.Meetups))
+  var groups = data.map(d => (d.Meetups))
 
   // Add X axis
-  const x = d3.scaleBand()
+  var x = d3.scaleBand()
       .domain(groups)
       .range([0, wi])
       .padding([0.2])
@@ -48,40 +49,39 @@ tooltip2.append('div')
     .call(d3.axisBottom(x).tickSizeOuter(0));
 
   // Add Y axis
-  const y = d3.scaleLinear()
+  var y = d3.scaleLinear()
     .domain([0, 120])
     .range([ he, 0 ]);
   svg4.append("g")
     .call(d3.axisLeft(y));
 
-  // color palette = one color per subgroup
-  const color = d3.scaleOrdinal()
+  // Create the color scheme
+  var color = d3.scaleOrdinal()
     .domain(subgroups)
     .range(['#5F939A','#D8AC'])
 
-  //stack the data? --> stack per subgroup
-  const stackedData = d3.stack()
+  // Stack the data per subgroup
+  var stackedData = d3.stack()
     .keys(subgroups)
     (data)
 
-  //add axis titles
+  // Add axis titles
   svg4.append("text")
     .attr("x", 170)
     .attr("y", 5)
     .style("text-anchor", "start")
-    .text("Matches Per Day")
-  
-  
+    .text("Meetups")
+    .attr('id','highlight')
 
-  // Show the bars
+  // Show the bars and store for linking
   bars = svg4.append("g")
     .selectAll("g")
-    // Enter in the stack data = loop key per key = group per group
+    // Enter in the stack data and loop group per group
     .data(stackedData)
     .join("g")
       .attr("fill", d => color(d.key))
       .selectAll("rect")
-      // enter a second time = loop subgroup per subgroup to add all rectangles
+      // Anter again and loop subgroup per subgroup to add all rects
       .data(d => d)
       .enter()
       .append("rect")
@@ -89,6 +89,7 @@ tooltip2.append('div')
         .attr("y", d => y(d[1]))
         .attr("height", d => y(d[0]) - y(d[1]))
         .attr("width",x.bandwidth())
+        // COlor change on mouseover transition effect + add tooltip
         .on('mouseover',function(event,d){
           if (d[0]>0){
             var key1 = 'Female'
@@ -101,7 +102,7 @@ tooltip2.append('div')
           tooltip2.select('.label2').html('Meetups: '+d.data.Meetups);
           tooltip2.select('.count').html('Count: '+count);
           tooltip2.style('opacity',1);
-          d3.select(this).transition().duration(300).style('fill','#b8deea')
+          d3.select(this).transition().duration(300).style('fill','#b6cf9d')
         })
         .on('mousemove', function(event,d) {
           tooltip2.style("left", (event.x)+"px") 
@@ -120,26 +121,31 @@ tooltip2.append('div')
     
 })
 
+// Locate our graph info
 let bars
 
+// Linking function
 function updateChart1(age,reason){
   console.log(bars.select('g'))
-  d3.csv("data/draft.csv",type).then(function(data2) {
-    var meets = d3.rollup(data2, v => d3.sum(v, d => d.Matches), d => d.Reason, d => d.Age_Range);
+  d3.csv("data/online_dating.csv",type).then(function(data2) {
+    var meets = d3.rollup(data2, v => d3.sum(v, d => d.Meetups), d => d.Reason, d => d.Age_Range);
     var lens = d3.rollup(data2, v => v.length, d => d.Reason, d => d.Age_Range);
     var mean = meets.get(reason).get(age)/lens.get(reason).get(age)
     console.log(mean)
+    // We highlight the mean value of the corresponding age range + reason group
+    var result = 0;
     if (mean==0){
-      console.log(0)
+      var result = 0;
     } else if (0<mean && mean<=3){
-      console.log(3)
+      var result = 3;
     } else if (3<mean && mean<=8){
-      console.log(8)
+      var result = 8;
     } else if (8<mean && mean<=15){
-      console.log(15)
+      var result = 15;
     }else{
-      console.log(20)
+      var result = 20;
     }
+    console.log(result)
     
 
 })
--- js/visualization.js
+++ js/visualization.js
@@ -1,34 +1,34 @@
-// Immediately Invoked Function Expression to limit access to our 
-// variables and prevent 
+// Vis 1 --> Reason + age ranges
 
+// Parameters for interaction animation
 var myDuration = 600;
 var firstTime = true;
 
-var width = 960,
-height = 500,
-radius = Math.min(width, height) / 2;
-
+// Pie parameters + selected color scheme
 var width = 960,
 height = 500,
 radius = Math.min(width, height) / 2;
 var color = d3.scaleOrdinal(['#EBB9DF','#60992D','#D991BA','#8CAE68']);
 
+// Pie variable function with data counts
 var pie = d3.pie()
 .value(function(d) { return d.Count; })
 .sort(null);
 
+// Pie arcs variable
 var arc = d3.arc()
 .innerRadius(radius - 100)
 .outerRadius(radius - 20);
 
+// Select where we want to place our vis and create svg space
 var svg = d3.select("#vis-svg-1")
 .attr("width", width-400)
 .attr("height", height)
 .append("g")
-
 .attr("transform", "translate(" + width / 3.7 + "," + height / 2.1 + ")");
 
-  d3.csv("data/draft.csv",type).then((data_pie, error) => {
+// Read the data and group according to Reason and Age_Range, then count observations in these groups and create dataframe
+  d3.csv("data/online_dating.csv",type).then((data_pie, error) => {
     var reasonByAge = d3.rollup(data_pie, v => v.length, d => d.Reason, d => d.Age_Range);
     var dict = [{Age_Range: '18-19', Reason: 'Entertainment', Count: reasonByAge.get('Entertainment').get('18-19')},
                 {Age_Range: '20-21', Reason: 'Entertainment', Count: reasonByAge.get('Entertainment').get('20-21')},
@@ -47,12 +47,14 @@ var svg = d3.select("#vis-svg-1")
                 {Age_Range: '22-23', Reason: 'For casual hookups', Count: reasonByAge.get('For casual hookups').get('22-23')},
                 {Age_Range: '24-26', Reason: 'For casual hookups', Count: reasonByAge.get('For casual hookups').get('24-26')}]
     
+    // Obtain input selection from user and update data accordingly
     d3.selectAll('input[name="reason"]')
     .on('change',function(){
       var data_filtered = dict.filter(d => (d.Reason == this.value))
       change(data_filtered)
     })
 
+    // Tooltip creation
     var tooltip = d3.select('#vis1')
     .append('div')
     .style('opacity','0')
@@ -68,40 +70,50 @@ var svg = d3.select("#vis-svg-1")
     tooltip.append('div')
     .attr('class', 'count');
 
+    // Function for the change of data
     function change(Age_Range) {
+          // Obtain previous and new data to be shown
           var path = svg.selectAll("path");
           var data0 = path.data(),
           data1 = pie(Age_Range);
     
           path = path.data(data1, key);
 
+          // On mouseover, show tooltip and transition for effect of shadow
           path.on('mouseover', function(event,d) {
             tooltip.select('.label1').html('Age Range: '+d.data.Age_Range).style('color','black');
             tooltip.select('.count').html('Count: '+ d.data.Count);
             tooltip.select('.label2').html('Reason: '+d.data.Reason).style('color','black');
             tooltip.style('opacity',1);
-            d3.select(this).transition().duration(300).style('fill','#b8deea')
+            d3.select(this).transition().duration(300)
+              .style('filter','drop-shadow(2px 4px 7px black)')
+            // Store selection for linking
             var age = d.data.Age_Range;
             var reason = d.data.Reason;
             updateChart1(age,reason);
       
           });
-      
+          
+          // Move tooltip accordingly when the mouse is moved
           path.on('mousemove', function(event,d) {
             tooltip.style("left", (event.x)+"px") 
             .style("top", (event.y+700) +"px");
           });
-      
+          
+          // Hide tooltip and transition when mouse is removed from slice
           path.on('mouseout', function(event,d) {
             tooltip.style('opacity',0);
-            d3.select(this).transition().duration(300).style('fill',color(d.data.Age_Range))
+            d3.select(this).transition().duration(300)
+              .style('filter','none')
           });
-    
+          
+          // Transition into new data
           path
           .transition()
           .duration(myDuration)
           .attrTween("d", arcTween)
 
+          // Arc adjusting for smooth effect
           path
               .enter()
               .append("path")
@@ -143,6 +155,7 @@ var svg = d3.select("#vis-svg-1")
               
   }) 
   
+  // Helper functions to obtain data
   function key(d) {
     return d.data.Age_Range;
   }
@@ -152,6 +165,7 @@ var svg = d3.select("#vis-svg-1")
     return d;
   }
 
+  // Helper functions to find the best way to arrange the information
   function findNeighborArc(i, data0, data1, key) {
     var d;
     if(d = findPreceding(i, data0, data1, key)) {
@@ -214,16 +228,14 @@ function cloneObj(obj) {
   return o;
 }
 
-// Vis 3
-// set the dimensions and margins of the graph
+// Vis 3 --> Vices
+// Set the dimensions and margins of the graph
 var width = 450,
     height = 450,
     margin = 40;
-
-// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
 var radius = Math.min(width, height) / 2 - margin;
 
-// append the svg object to the div
+// Append the svg object to the div for pie 1
 var svg1 = d3.select("#my_dataviz")
   .append("svg")
     .attr("width", width)
@@ -232,7 +244,7 @@ var svg1 = d3.select("#my_dataviz")
   .append("g")
     .attr("transform", `translate(${width/2}, ${height/2})`);
 
-// append the svg object to the div
+// Append the svg object to the div for pie 2
 var svg2 = d3.select("#my_dataviz")
 .append("svg")
   .attr("width", width)
@@ -241,16 +253,15 @@ var svg2 = d3.select("#my_dataviz")
 .append("g")
   .attr("transform", `translate(${width/2}, ${height/2})`);
 
-// Set colors
-
+// Set color scheme
 var color1 = d3.scaleOrdinal()
-  //.domain(Object.keys(data))
   .range(["#FCECDD", "#FFC288", "#FEA82F", "#FF6701", "#FF8303", "#A35709", "#4E3620"])
 
 // Create pie
 var pie = d3.pie()
   .value(function(d) {return d.Count})
 
+// Create tooltip
 var tooltip1 = d3.select('#my_dataviz')                               
   .append('div')    
   .style('opacity','0')                                            
@@ -263,6 +274,7 @@ tooltip1.append('div')
   .attr('class', 'count');                                      
 
 
+// Read data
 d3.csv('data/Turn-Ons.csv').then(function(data){
   
 // Build the pie chart where each part of the pie is a path that we build using the arc function.
@@ -278,11 +290,12 @@ svg1
   .attr("stroke", "black")
   .style("stroke-width", "2px")
   .style("opacity", 0.7)
+  // Same affects as in the first pie chart
   .on('mouseover',function(event,d){
     tooltip1.select('.label1').html('Attribute: '+d.data.Categories).style('color','black');
     tooltip1.select('.count').html('Count: '+d.data.Count);
     tooltip1.style('opacity',1);
-    d3.select(this).transition().duration(300).style('fill','#b8deea')
+    d3.select(this).transition().duration(300).style('filter','drop-shadow(2px 4px 7px black)')
   })
   .on('mousemove', function(event,d) {
     tooltip1.style("left", (event.x)+"px") 
@@ -291,12 +304,13 @@ svg1
   })
   .on('mouseout', function() {
     tooltip1.style('opacity',0);
-    d3.select(this).transition().duration(300).style('fill',function(d){ return(color1(d.data.Categories)) })
+    d3.select(this).transition().duration(300).style('filter','none')
   });
 
 
 })
 
+// Same procedure as the turn-ons data
 d3.csv('data/Turn-Offs.csv').then(function(data){
   
 // Build the pie chart where each part of the pie is a path that we build using the arc function.
@@ -316,7 +330,7 @@ svg2
     tooltip1.select('.label1').html('Attribute: '+d.data.Categories).style('color','black');
     tooltip1.select('.count').html('Count: '+d.data.Count);
     tooltip1.style('opacity',1);
-    d3.select(this).transition().duration(300).style('fill','#b8deea')
+    d3.select(this).transition().duration(300).style('filter','drop-shadow(2px 4px 7px black)')
   })
   .on('mousemove', function(event,d) {
     tooltip1.style("left", (event.x)+"px") 
@@ -325,7 +339,7 @@ svg2
   })
   .on('mouseout', function() {
     tooltip1.style('opacity',0);
-    d3.select(this).transition().duration(300).style('fill',function(d){ return(color1(d.data.Categories)) })
+    d3.select(this).transition().duration(300).style('filter','none')
   });
 
   var mySvg = d3.select('#my_dataviz')
@@ -335,14 +349,16 @@ svg2
       .attr('id','legend');
 
 
-  var legendG = mySvg.selectAll(".legend") // note appending it to mySvg and not svg to make positioning easier
+  // Create a separate element for the legend and pass data
+  var legendG = mySvg.selectAll(".legend") 
   .data(pie(data))
   .enter().append("g")
   .attr("transform", function(d, i) {
-     return "translate(" + (width - 390) + "," + (i * 24 + 10) + ")"; // place each legend on the right and bump each one down 15 pixels
+     return "translate(" + (width - 390) + "," + (i * 24 + 10) + ")"; // place each legend on the right and bump each one down 24 pixels
   })
   .attr("class", "legend");
 
+// Create color elements as rects and set corresponding color
 legendG.append("rect") // make a matching color rect
   .attr("width", 16)
   .attr("height", 16)
@@ -353,6 +369,7 @@ legendG.append("rect") // make a matching color rect
      return color1(d.data.Categories);
   });
 
+// Create text elements and set corresponding attribute name
 legendG.append("text") // add the text
   .text(function(d) {
      return d.data.Categories;
